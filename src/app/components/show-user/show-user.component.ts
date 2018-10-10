dedf816dc7ded5e79382fcef451ec86d0d3f4b32
RAJAT SINGH <rajasing@localhost.localdomain>
2018-10-10 14:33:54 +0530

    added sorting by name/email

--- src/app/components/show-user/show-user.component.ts
+++ src/app/components/show-user/show-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataStoreService } from '../../services/data-store.service';
+import { SearchUserService } from '../../services/search-user.service';
+import { log } from 'util';
 
 @Component({
   selector: 'app-show-user',
@@ -7,12 +9,25 @@ import { DataStoreService } from '../../services/data-store.service';
   styleUrls: ['./show-user.component.css']
 })
 export class ShowUserComponent implements OnInit {
-
+  users;
   constructor(
-    private store: DataStoreService
+    private store: DataStoreService,
+    private searchuserservices: SearchUserService
   ) { }
-
+  sortedArray: any;
+  sort(sortBy: string, order: string) {
+    if (order === 'desc') {
+      this.users.sort(function(a, b) {
+        return b.attributes[sortBy].localeCompare(a.attributes[sortBy], 'en', {sensitivity: 'base'}); // descending
+      });
+    } else if (order === 'asc') {
+      this.users.sort(function(a, b) {
+        return a.attributes[sortBy].localeCompare(b.attributes[sortBy], 'en', {sensitivity: 'base'}); // descending
+      });
+    }
+  }
   ngOnInit() {
+    this.store.users.subscribe(users => this.users = users);
   }
 
 }
