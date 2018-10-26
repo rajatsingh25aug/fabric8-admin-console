7b63531f850cb73b069abbbae23eedf96dcb8cc7
RAJAT SINGH <rsinghmn@localhost.localdomain>
2018-10-26 20:51:28 +0530

    testing filtering

--- src/app/components/users-container/users-list/users-list.component.ts
+++ src/app/components/users-container/users-list/users-list.component.ts
@@ -56,6 +56,7 @@ export class UsersListComponent implements OnInit {
       weekDay: 'Monday',
     }];
     this.items = this.allItems;
+    this.items = Object.assign({}, this.users);
     this.filterConfig = {
       fields: [{
         id: 'name',
@@ -68,23 +69,8 @@ export class UsersListComponent implements OnInit {
         placeholder: 'Filter by Address...',
         type: FilterType.TEXT
       }] as FilterField[],
-      resultsCount: this.items.length,
       appliedFilters: []
     } as FilterConfig;
-
-    this.sortConfig = {
-      fields: [{
-        id: 'name',
-        title: 'Name',
-        sortType: 'alpha'
-      }, {
-        id: 'email',
-        title: 'Email',
-        sortType: 'alpha'
-      }],
-      isAscending: this.isAscendingSort
-    } as SortConfig;
-
     this.toolbarConfig = {
       filterConfig: this.filterConfig,
       sortConfig: this.sortConfig
@@ -121,6 +107,7 @@ export class UsersListComponent implements OnInit {
 
   // Handle filter changes
   filterChanged($event: FilterEvent): void {
+    console.log(this.items);
     this.filtersText = '';
     $event.appliedFilters.forEach((filter) => {
       this.filtersText += filter.field.title + ' : ' + filter.value + '\n';
@@ -148,29 +135,6 @@ export class UsersListComponent implements OnInit {
       }
     });
     return matches;
-  }
-
-  // Sort
-
-  compare(item1: any, item2: any): number {
-    let compValue = 0;
-    if (this.currentSortField.id === 'name') {
-      console.log(this.users.length);
-      compValue = item1.name.localeCompare(item2.name);
-    } else if (this.currentSortField.id === 'address') {
-      compValue = item1.address.localeCompare(item2.address);
-    }
-    if (!this.isAscendingSort) {
-      compValue = compValue * -1;
-    }
-    return compValue;
-  }
-
-  // Handle sort changes
-  sortChanged($event: SortEvent): void {
-    this.currentSortField = $event.field;
-    this.isAscendingSort = $event.isAscending;
-    this.items.sort((item1: any, item2: any) => this.compare(item1, item2));
   }
 
 
