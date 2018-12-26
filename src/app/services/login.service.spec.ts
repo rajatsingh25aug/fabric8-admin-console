8a45ac043ee01ee99269bd8d1856d67602c8ea00
Rajat Singh <rajatsingh9bdpsalld@gmail.com>
2018-12-26 16:39:15 +0530

    userStore test

--- src/app/services/login.service.spec.ts
+++ src/app/services/login.service.spec.ts
@@ -7,28 +7,35 @@ import { AUTH_API_URL, SSO_API_URL, WIT_API_PROXY, REALM, UserService, Authentic
 import { ADMIN_API_URL } from '../shared/admin-api';
 import { Broadcaster, Logger } from 'ngx-base';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('LoginService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
+  beforeEach(() => {
+     TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ],
     providers: [
-      { provide: AUTH_API_URL, useValue: 'https://auth.example.com/api/' },
-      { provide: SSO_API_URL, useValue: 'https://sso.example.com/auth/api/' },
-      { provide: WIT_API_PROXY, useValue: 'https://wit.example.com/api/'},
-      { provide: ADMIN_API_URL, useValue: 'https://admin.example.com/api/'},
-      { provide: REALM, useValue: 'realm' },
-      Broadcaster,
-      Logger,
-      UserService,
       AuthenticationService,
-      {
-        provide: Router
-      }
+      Broadcaster,
+      { provide: AUTH_API_URL, useValue: 'https://auth.example.com' },
+      { provide: SSO_API_URL, useValue: 'https://sso.example.com' },
+      { provide: WIT_API_PROXY, useValue: 'https://wit.example.com' },
+      { provide: REALM, useValue: 'realm' },
+    HttpClient,
+    HttpHandler,
+    {provide: Router}
     ]
-  }));
+  });
+
+});
 
   it('should be created', () => {
     const service: LoginService = TestBed.get(LoginService);
     expect(service).toBeTruthy();
+  });
+  it('should redirect to Auth', () => {
+    const service: LoginService = TestBed.get(LoginService);
+    expect(service.redirectToAuth).toBe('test');
   });
 });
